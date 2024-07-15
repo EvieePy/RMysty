f838b1e5471d268737ff782c91e54ff522848796
Eviee Py <evieepy@gmail.com>
2024-07-16 03:45:52 +1000

    Add simple app_command logs

--- extensions/admin.py
+++ extensions/admin.py
@@ -13,15 +13,30 @@ See the License for the specific language governing permissions and
 limitations under the License.
 """
 
-from typing import Literal
+from __future__ import annotations
+
+from typing import TYPE_CHECKING, Any, Literal
 
 import discord
+from discord import app_commands
 from discord.ext import commands
 
-import core
+
+if TYPE_CHECKING:
+    import core
+
+
+type CommandT = app_commands.Command[Admin, ..., Any] | app_commands.ContextMenu
 
 
 class Admin(commands.Cog):
+    def __init__(self, bot: core.Bot) -> None:
+        self.bot: core.Bot = bot
+        self.logs: discord.TextChannel | None = None
+
+    async def cog_load(self) -> None:
+        self.logs = await self.bot.fetch_channel(1262457734746472508)  # type: ignore
+
     @commands.command()
     @commands.guild_only()
     @commands.is_owner()
@@ -57,6 +72,17 @@ class Admin(commands.Cog):
                 ret += 1
 
         await ctx.send(f"Synced the tree to {ret}/{len(guilds)}.")
+
+    @commands.Cog.listener()
+    async def on_app_command_completion(self, interaction: discord.Interaction[core.Bot], command: CommandT) -> None:
+        if not self.logs:
+            return
+
+        user: discord.Member | discord.User = interaction.user
+        channel: str = str(interaction.channel) if interaction.channel else "DM"
+        message = f"`{user} ({user.id})` used `{command.qualified_name}` in **`{interaction.guild} - (#{channel})`**"
+
+        await self.logs.send(message)
 
 
 async def setup(bot: core.Bot) -> None:
