474590e71c63d8881a6bfd6d8906888d17374853
Eviee Py <29671945+EvieePy@users.noreply.github.com>
2024-11-24 19:53:37 +1000

    Fix discord formatting issue

--- extensions/random.py
+++ extensions/random.py
@@ -43,14 +43,14 @@ class Random(commands.Cog):
             await interaction.followup.send("Cancelling countdown!")
             return
 
-        msg_: discord.WebhookMessage = await interaction.followup.send("### 3")  # type: ignore
+        msg_: discord.WebhookMessage = await interaction.followup.send("**3**")  # type: ignore
         await asyncio.sleep(1)
 
         for i in range(2, 0, -1):
-            await msg_.edit(content=f"### {i}")
+            await msg_.edit(content=f"**{i}**")
             await asyncio.sleep(1)
 
-        await msg_.edit(content="## Go!")
+        await msg_.edit(content="**Go!**")
 
 
 async def setup(bot: core.Bot) -> None:
